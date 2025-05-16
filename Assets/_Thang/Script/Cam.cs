e949483286a4fbf7dbda5ec2bf77a13430d076e8
Thuanab\VThuan <thuanab12@gmail.com>
2025-05-16 11:16:51 +0700

    Track texture file with Git LFS

---
+++ Assets/_Thang/Script/Cam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public Transform car;
+    public Vector3 offset = new Vector3(0, 2, -5);
+
+    void LateUpdate()
+    {
+        if (car == null) return;
+
+        // Di chuyển camera theo xe
+        transform.position = car.TransformPoint(offset);
+
+        // Lấy góc xoay của xe nhưng bỏ trục X và Z (chỉ giữ trục Y)
+        Vector3 carEuler = car.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, carEuler.y, 0f);
+    }
+}
