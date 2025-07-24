f20a071a44c63fc4c2ed07f63173884172757cd6
tudeptai <tuhapk03696@gmail.com>
2025-07-24 18:43:59 +0700

    + add hiệu ứng hover button

---
+++ Assets/_Tu/Scrip/ButtonHoverEffect.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private Vector3 originalScale;
+    public float scaleMultiplier = 1.1f;
+    public float animationSpeed = 10f;
+    private bool isHovered = false;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Vector3 targetScale = isHovered ? originalScale * scaleMultiplier : originalScale;
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * animationSpeed);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovered = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click!"); // Dòng kiểm tra click có hoạt động không
+
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource hoặc AudioClip chưa được gán!");
+        }
+    }
+}
---
+++ Assets/_Tu/Scrip/ShowSpinEffect.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class ShowSpinEffect : MonoBehaviour
+{
+    public Vector3 startPositionOffset = new Vector3(-1000f, 0f, 0f); // bay từ trái
+    public float appearDuration = 0.5f;
+    public AnimationCurve moveCurve;
+    public AnimationCurve scaleCurve;
+
+    private Vector3 originalPosition;
+    private Vector3 originalScale;
+
+    void Awake()
+    {
+        originalPosition = transform.localPosition;
+        originalScale = transform.localScale;
+
+        // Ẩn ban đầu (đặt ở vị trí lệch + nhỏ lại)
+        transform.localPosition = originalPosition + startPositionOffset;
+        transform.localScale = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(AnimateAppear());
+    }
+
+    private System.Collections.IEnumerator AnimateAppear()
+    {
+        float time = 0f;
+
+        while (time < appearDuration)
+        {
+            float t = time / appearDuration;
+
+            // Áp dụng animation curve nếu có
+            float moveT = moveCurve != null ? moveCurve.Evaluate(t) : t;
+            float scaleT = scaleCurve != null ? scaleCurve.Evaluate(t) : t;
+
+            transform.localPosition = Vector3.Lerp(originalPosition + startPositionOffset, originalPosition, moveT);
+            transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, scaleT);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
+        transform.localScale = originalScale;
+    }
+}
